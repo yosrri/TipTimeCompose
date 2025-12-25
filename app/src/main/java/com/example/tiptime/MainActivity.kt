8890b3596ed41df978b25df99f922a3776315c16
Yosri <yosrri11@gmail.com>
2025-12-25 14:57:40 +0200

    Make calculateTip internal and visible for testing
    Added @VisibleForTesting and changed calculateTip to internal for improved testability. Also updated EditNumberField to use a lambda for onValueChange.

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -153,7 +154,7 @@ fun EditNumberField(
 ) {
     TextField(
         value = value,
-        onValueChange = onValueChange,
+        onValueChange = {onValueChange(it)},
         label = { Text(text = stringResource(label)) },
         leadingIcon = {
             Icon(
@@ -195,7 +196,8 @@ fun RoundTheTipRow(
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+@VisibleForTesting
+internal fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
     var tip = tipPercent / 100 * amount
     if (roundUp)
         tip = ceil(tip)
